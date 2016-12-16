32e70d04e917863dad719ea605c8b28e5139268c
Idir Lankri <lankri.idir@gmail.com>
2016-12-16 09:49:34 +0100

    Fix mistake in color recognition main function

--- src/color_recognition.ml
+++ src/color_recognition.ml
@@ -9,26 +9,26 @@ let what_color () =
     Color_sensor.reflected_light () in
   if c = 100 then Color.White else
   if c < 8 then Color.Black else
-  let r, g, b =
-    Color_sensor.set_mode Color_sensor.RGB_RAW;
-    Color_sensor.raw_color_components ()
-  in
-  let r, g, b = Color.(lerp_red r, lerp_green g, lerp_blue b) in
-  let hue = Color.(hue_of_rgb {red = r; green = g; blue = b}) in
-  let f (old_dist, old_hue) x =
-    let new_dist =
-      Math.dist hue x in
-    if new_dist < old_dist then (new_dist, x) else (old_dist, old_hue)
-  in
-  let _, res = List.fold_left f (360, -1) Color.hue_reflist in
-  Printf.printf "hue = %d\n" hue;
-  Color.color_of_hue res
+    let r, g, b =
+      Color_sensor.set_mode Color_sensor.RGB_RAW;
+      Color_sensor.raw_color_components ()
+    in
+    let r, g, b = Color.(lerp_red r, lerp_green g, lerp_blue b) in
+    let hue = Color.(hue_of_rgb {red = r; green = g; blue = b}) in
+    let f (old_dist, old_hue) x =
+      let new_dist =
+        Math.dist hue x in
+      if new_dist < old_dist then (new_dist, x) else (old_dist, old_hue)
+    in
+    let _, res = List.fold_left f (360, -1) Color.hue_reflist in
+    Printf.printf "hue = %d\n" hue;
+    Color.color_of_hue res
 
 let main () =
-  let raw_color =
+  let color =
     Color_sensor.connect();
+    what_color ()
   in
-  let color = what_color raw_color in
   print_endline Color.(string_of_color color);
   Color_sensor.disconnect ();
   (* Unix.sleep 3; *)
