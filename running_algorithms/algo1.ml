683edde03f1b0d83a1175daea56bf43ba3e8548c
AMRIOUI Yani <amrioui@informatique.univ-paris-diderot.fr>
2017-02-19 16:30:29 +0100

    trying to write a program for algorithm 1

---
+++ running_algorithms/algo1.ml
@@ -0,0 +1,61 @@
+let prefix = "/sys/class/tacho-motor/"
+
+let line_color = ref (Color_recognition.read_color()) (* type representing the color *)
+
+module MotorA_path_finder =
+  Path_finder.Make_absolute(struct
+    let path = prefix ^ "motor0/"
+  end)
+
+module MotorB_path_finder =
+  Path_finder.Make_absolute(struct
+    let path = prefix ^ "motor1/"
+  end)
+
+module MotorA_dev =
+  Device.Make_device (struct
+    let name = "motorA" let multiple_connection = true
+  end) (MotorA_path_finder)
+
+module MotorB_dev = Device.Make_device (struct
+    let name = "motorB"
+    let multiple_connection = true
+  end) (MotorB_path_finder)
+
+module Color_sensor_path_finder =
+  Path_finder.Make_absolute(struct
+    let path = "/sys/class/lego-sensor/sensor0/"
+  end)
+
+module Color_sensor_dev =
+  Device.Make_device (struct
+    let name= "color-sensor" let multiple_connection=true
+  end) (Color_sensor_path_finder)
+
+let main () =
+  let set_time_sp i =
+    let filename = "time_sp" in
+    MotorA_dev.action_write_int i filename;
+    MotorB_dev.action_write_int i filename in
+  let set_duty_cycle_sp i =
+    let filename = "duty_cycle_sp" in
+    MotorA_dev.action_write_int i filename;
+    MotorB_dev.action_write_int i filename in
+  let set_command s =
+    let filename = "command" in
+    MotorA_dev.action_write_string s filename;
+    MotorB_dev.action_write_string s filename
+  in
+  MotorA_dev.connect ();
+  MotorB_dev.connect ();
+  Color_sensor_dev.connect();
+  set_time_sp 10000;
+  set_speed 20;
+  set_command "run-forever";
+  (* run until the color changes *)
+  MotorA_dev.disconnect ();
+  MotorB_dev.disconnect ();
+  Color_sensor_dev.disconnect();
+  exit 0
+
+let _ = main ()
